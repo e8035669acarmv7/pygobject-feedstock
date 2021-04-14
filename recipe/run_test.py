43939f2f898ca98d341ff8d1ff581ebd0179f63b
Ryan Volz <ryan.volz@gmail.com>
2021-04-14 16:59:35 -0400

    Drop testing dep on gtk3 to avoid circular dependencies.
    gtk3 depends on pygobject at testing time for checking its GI typelibs,
    so having pygobject also depend on gtk3 here creates a circular
    dependency. It makes the most sense to have pygobject be the lower level
    dependency and let gtk3 continue to do its GI typelib check depending on
    pygobject.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,8 +1,6 @@
 import gi
 gi.require_version("GLib", "2.0")
-gi.require_version("Gtk", "3.0")
 assert("__init__" in gi.__file__)
-from gi.repository import GLib, Gtk
+from gi.repository import GLib
 assert(31 == GLib.Date.get_days_in_month(GLib.DateMonth.JANUARY, 2000))
-assert(3 == Gtk.get_major_version())
 
